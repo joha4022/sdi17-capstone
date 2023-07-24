89445899638e45311bfee73cd45e0dbc292b70c8
joha4022 <joha4022@gmail.com>
2023-07-24 08:38:43 -0700

    created base for front end

---
+++ front/sme/src/Components/LoginPage.js
@@ -0,0 +1,9 @@
+import { Title } from '../Styled';
+
+export default function LoginPage() {
+  return(
+    <>
+      <Title>hello</Title>
+    </>
+  )
+}
---
+++ front/sme/src/Styled.js
@@ -0,0 +1,10 @@
+import {styled} from 'styled-components';
+
+const Title = styled.h1`
+  font-weight: bold;
+`
+
+
+export {
+  Title
+}
