2b94419c56ef2bb4a25bce5f41f926375ffcb37c
athuro <alexanderthuro@gmail.com>
2023-07-25 11:55:01 -0500

    Added api call and network functionality

--- front/sme/src/Components/SME.js
+++ front/sme/src/Components/SME.js
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 
 const SME = () => {
+
+    const { id } = useParams();
+    console.log(id)
     return ('Hello from SME page')
 }
 
