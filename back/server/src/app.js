702829548a5ac4b4a309150911a61af90f9ac282
Miguel <angel.miguelr@yahoo.com>
2023-07-25 12:02:20 -0700

    changed the /sme route

--- back/server/src/app.js
+++ back/server/src/app.js
@@ -14,48 +14,49 @@ app.use(cors());
 
 // API returns everything sme with categories combined and certain info omitted
 app.get("/smes", (req, res) => {
-  knex("users")
-    .join("base", "users.base_id", "base.baseid")
-    .join("sme", "users.userid", "sme.user_id")
-    .join("network", "sme.user_id", "network.user_id")
-    .join("category", "sme.category_id", "category.categoryid")
-    .select(
-      "users.userid",
-      "users.firstname",
-      "users.lastname",
-      "users.email",
-      "users.phonenumber",
-      "users.photo",
-      "base.name AS base",
-      "base.branch",
-      knex.raw("ARRAY_AGG(category.name) AS categories")
-    )
-    .groupBy(
-      "users.userid",
-      "users.firstname",
-      "users.lastname",
-      "users.email",
-      "users.phonenumber",
-      "users.photo",
-      "base.name",
-      "base.branch"
-    )
-    .then((data) => {
-      const formattedData = data.map((item) => {
-        return {
-          ...item,
-          categories: item.categories, // categories will be returned as an array directly from the query
-        };
-      });
+    knex("users")
+        .join("base", "users.base_id", "base.baseid")
+        .join("sme", "users.userid", "sme.user_id")
+        .join("network", "sme.user_id", "network.user_id")
+        .join("category", "sme.category_id", "category.categoryid")
+        .select(
+            "users.userid",
+            "users.firstname",
+            "users.lastname",
+            "users.email",
+            "users.phonenumber",
+            "users.photo",
+            "users.branch",
+            "base.basename AS base",
+            knex.raw("ARRAY_AGG(category.categoryname) AS categories")
+        )
+        .groupBy(
+            "users.userid",
+            "users.firstname",
+            "users.lastname",
+            "users.email",
+            "users.phonenumber",
+            "users.photo",
+            "users.branch",
+            "base.basename",
+        )
+        .then((data) => {
+            const formattedData = data.map((item) => {
+                return {
+                    ...item,
+                    categories: item.categories, // categories will be returned as an array directly from the query
+                };
+            });
 
-      res.status(200).json(formattedData);
-    })
-    .catch((err) =>
-      res.status(404).json({
-        message:
-          "The data you are looking for could not be found. Please try again",
-      })
-    );
+            res.status(200).json(formattedData);
+        })
+        .catch((err) =>
+            res.status(404).json({
+                message:
+                    "The data you are looking for could not be found. Please try again",
+                error: err,
+            })
+        );
 });
 
 app.get('/', function (req, res) {
@@ -142,35 +143,60 @@ app.get('/all2', function (req, res) {
 
 //----------------------------------------------------------------------------------------------------------
 //API to get all users
-app.get('/profile/:userid', function(req, res) {
+app.get('/profile/:userid', function (req, res) {
     const userid = req.params.userid;
 
-            knex('users')
-                .select('users.userid',
-                        'users.firstname',
-                        'users.lastname',
-                        'users.username',
-                        'users.email',
-                        'users.supervisoremail',
-                        'users.approveremail',
-                        'users.phonenumber',
-                        'users.bio',
-                        'users.photo',
-                        'users.sme',
-                        'users.admin',
-                        )
+    knex('users')
+        .select('users.userid',
+            'users.firstname',
+            'users.lastname',
+            'users.username',
+            'users.email',
+            'users.supervisoremail',
+            'users.approveremail',
+            'users.phonenumber',
+            'users.bio',
+            'users.photo',
+            'users.sme',
+            'users.admin',
+        )
 
-                .then(data => res.status(200).json(data))
-                .catch(err =>
-                res.status(404).json({
-                    message:
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message:
                     'The data you are looking for could not be found. Please try again'
-                })
-                );
-            });
+            })
+        );
+});
 
-            app.post('/createuser', (req, res) => {
-                const { firstname,
+app.post('/createuser', (req, res) => {
+    const { firstname,
+        lastname,
+        username,
+        password,
+        email,
+        supervisoremail,
+        approveremail,
+        phonenumber,
+        bio,
+        photo,
+        sme,
+        admin
+    } = req.body;
+    console.log(firstname, lastname, username, password);
+    //let userid = 4
+    knex('users')
+        .select('username')
+        .where('username', username)
+        .then((data) => {
+            console.log('data length: ', data.length)
+            if (data.length > 0) {
+                res.status(404).json({ userCreated: false, message: `Username: *${username}* already taken!` });
+            } else {
+                knex('users')
+                    .insert({
+                        firstname,
                         lastname,
                         username,
                         password,
@@ -182,81 +208,56 @@ app.get('/profile/:userid', function(req, res) {
                         photo,
                         sme,
                         admin
-                    } = req.body;
-                console.log( firstname, lastname, username, password );
-                //let userid = 4
-                knex('users')
-                .select('username')
-                .where('username', username)
-                .then((data) => {
-                    console.log('data length: ', data.length)
-                    if (data.length > 0){
-                        res.status(404).json({userCreated: false, message: `Username: *${username}* already taken!`});
-                    }else{
-                        knex('users')
-                        .insert({
-                            firstname,
-                            lastname,
-                            username,
-                            password,
-                            email,
-                            supervisoremail,
-                            approveremail,
-                            phonenumber,
-                            bio,
-                            photo,
-                            sme,
-                            admin
-                         })
-                        .then(() => res.status(201).json({userCreated: true, message: 'Username created successfully'}))
-                    }
-                })
-                .catch((err) =>
-                    res.status(500).json({
-                    message: 'An error occurred while fetching the login',
-                    error: err,
                     })
-                );
-            });
-            app.delete('/deleteuser/:userid', function(req, res) {
-                const userid = req.params.userid;
-                console.log(userid)
-                knex('users')
-                    .where('userid', userid)
-                    .del()
-                    .then((rowCount) => {
-                        console.log("here")
-                        if (rowCount === 0) {
-                        return res.status(404).json({
-                            message: 'User not found',
-                        });
-                        }
-                        res.status(200).json({
-                        message: 'User deleted successfully',
-                        });
-                    })
-                    .catch((err) =>
-                        res.status(500).json({
-                        message: 'An error occurred while deleting the user',
-                        error: err,
-                        })
-                    );
+                    .then(() => res.status(201).json({ userCreated: true, message: 'Username created successfully' }))
+            }
+        })
+        .catch((err) =>
+            res.status(500).json({
+                message: 'An error occurred while fetching the login',
+                error: err,
+            })
+        );
+});
+app.delete('/deleteuser/:userid', function (req, res) {
+    const userid = req.params.userid;
+    console.log(userid)
+    knex('users')
+        .where('userid', userid)
+        .del()
+        .then((rowCount) => {
+            console.log("here")
+            if (rowCount === 0) {
+                return res.status(404).json({
+                    message: 'User not found',
                 });
+            }
+            res.status(200).json({
+                message: 'User deleted successfully',
+            });
+        })
+        .catch((err) =>
+            res.status(500).json({
+                message: 'An error occurred while deleting the user',
+                error: err,
+            })
+        );
+});
 
 
 
 
 // Check user name and password against database
-        app.post('/login/', (req, res) => {
-            const { user, pw} = req.body;
-            //console.log('req.body: ',req.body)
-            console.log('user password:', user, pw)
-            knex('users')
-            .select('userid' , 'firstname', 'lastname')
-            .where('username', user)
-            .where('password', pw)
-            .then((data) => {
-                if (data.length === 0) {
+app.post('/login/', (req, res) => {
+    const { user, pw } = req.body;
+    //console.log('req.body: ',req.body)
+    console.log('user password:', user, pw)
+    knex('users')
+        .select('userid', 'firstname', 'lastname')
+        .where('username', user)
+        .where('password', pw)
+        .then((data) => {
+            if (data.length === 0) {
                 return res.status(404).json({
                     message: 'User name and/or passowrd are incorrect',
                 });
@@ -273,27 +274,27 @@ app.get('/profile/:userid', function(req, res) {
 
 // Check user name and password against database
 app.post("/login/", (req, res) => {
-  const { user, pw } = req.body;
-  //console.log('req.body: ',req.body)
-  console.log("user password:", user, pw);
-  knex("users")
-    .select("userid", "firstname", "lastname")
-    .where("username", user)
-    .where("password", pw)
-    .then((data) => {
-      if (data.length === 0) {
-        return res.status(404).json({
-          message: "User name and/or passowrd are incorrect",
-        });
-      }
-      res.status(200).json(data);
-    })
-    .catch((err) =>
-      res.status(500).json({
-        message: "An error occurred while fetching the login",
-        error: err,
-      })
-    );
+    const { user, pw } = req.body;
+    //console.log('req.body: ',req.body)
+    console.log("user password:", user, pw);
+    knex("users")
+        .select("userid", "firstname", "lastname")
+        .where("username", user)
+        .where("password", pw)
+        .then((data) => {
+            if (data.length === 0) {
+                return res.status(404).json({
+                    message: "User name and/or passowrd are incorrect",
+                });
+            }
+            res.status(200).json(data);
+        })
+        .catch((err) =>
+            res.status(500).json({
+                message: "An error occurred while fetching the login",
+                error: err,
+            })
+        );
 });
 
 app.listen(PORT, () => {
