adc442269ad15bb60a1ad0f7f8a8d9adae06a572
Jon <sauljm@earthlink.net>
2023-07-26 15:24:28 -0400

    Add user API to only return usernames

--- back/server/seeds/04_sme_table.js
+++ back/server/seeds/04_sme_table.js
@@ -6,9 +6,14 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('sme').del()
   await knex('sme').insert([
-    {smeid: 1, user_id: 1, category_id: 1},
-    {smeid: 2, user_id: 1, category_id: 2},
-    {smeid: 3, user_id: 2, category_id: 2},
-    {smeid: 4, user_id: 2, category_id: 3},
+    {user_id: 1, category_id: 1},
+    {user_id: 1, category_id: 2},
+    {user_id: 2, category_id: 2},
+    {user_id: 2, category_id: 3},
+    {user_id: 3, category_id: 5},
+    {user_id: 3, category_id: 6},
+    {user_id: 3, category_id: 7},
+    {user_id: 4, category_id: 1},
+
   ]);
 };
